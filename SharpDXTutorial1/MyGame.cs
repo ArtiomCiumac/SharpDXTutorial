36d0cdd341eb121759d807573331bd59cc433a84
ArtiomCiumac <artiom.ciumac@gmail.com>
2014-02-10 21:14:54 +0200

    [Tutorial] Added the projet for the first tutorial.

---
+++ SharpDXTutorial1/MyGame.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpDXTutorial1
+{
+    using SharpDX;
+    using SharpDX.Toolkit;
+
+    internal sealed class MyGame : Game
+    {
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+
+        public MyGame()
+        {
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SharpDXTutorial1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SharpDXTutorial1
+{
+    using System;
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MyGame())
+                game.Run();
+        }
+    }
+}
