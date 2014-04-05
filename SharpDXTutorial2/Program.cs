3bef22c64ebe01edcb2b6945a4209652ad1389aa
Artiom Ciumac <artiom.ciumac@gmail.com>
2014-04-05 22:35:55 +0100

    [Tutorial] Added the second tutorial project.

---
+++ SharpDXTutorial2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpDXTutorial2
+{
+    using System;
+
+    /// <summary>
+    /// Provides the entry point for the application.
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// The entry point of the aplication.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MyGame())
+                game.Run();
+        }
+    }
+}
